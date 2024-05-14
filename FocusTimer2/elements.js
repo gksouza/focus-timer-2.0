85bef3bd0036e370edb96c3332150e42d7402963
gksouza <gkoliver2020@gmail.com>
2024-05-14 14:52:20 -0300

    realocated files

---
+++ FocusTimer2/elements.js
@@ -0,0 +1,23 @@
+let timerMinutes = document.querySelector('#minutes')
+let timerSeconds = document.querySelector('#seconds')
+
+const btnPlay = document.querySelector('#play')
+const btnPause = document.querySelector('#pause')
+const btnStop = document.querySelector('#stop')
+const btnPlus = document.querySelector('#plus')
+const btnMinus = document.querySelector('#minus')
+
+const cardForest = document.querySelector('#forest')
+const cardRain = document.querySelector('#rain')
+const cardCoffee = document.querySelector('#coffee')
+const cardFire = document.querySelector('#fireplace')
+
+export {
+  btnPlay,
+  btnPause,
+  btnStop,
+  btnPlus,
+  btnMinus,
+  timerMinutes,
+  timerSeconds
+}
